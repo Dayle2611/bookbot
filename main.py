e9a0b3e7d23196f7865cfdd9dd58daa93ead82d7
Dayle2611 <Dwatson1191@gmail.com>
2025-03-01 23:16:02 +0000

    Add stats.py and other untracked files

---
+++ main.py
@@ -0,0 +1,14 @@
+from stats import get_num_words
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    bookAsString = get_book_text("./books/frankenstein.txt")
+    wordCount = get_num_words(bookAsString)
+    print(f'{wordCount} words found in the document')
+
+if __name__=="__main__":
+    main()
---
+++ stats.py
@@ -0,0 +1,5 @@
+def get_num_words(stringText):
+    wordList = stringText.split()
+    wordCount = len(wordList) 
+    return wordCount
+    
