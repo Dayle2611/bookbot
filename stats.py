fe286146617bf22f35e236cc7d15a34ddd301beb
Dayle Watson <daylewatson@Dayles-MacBook-Air.local>
2025-03-02 22:45:53 +0000

    finiding function for unique chars

--- stats.py
+++ stats.py
@@ -2,4 +2,8 @@ def get_num_words(stringText):
     wordList = stringText.split()
     wordCount = len(wordList) 
     return wordCount
-    
+
+def get_unique_chars(stringText):
+    unique = set(stringText)
+    return unique
+
